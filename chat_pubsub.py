3b72b9c211ba5c41ff7c0d522725f23ad7d4f945
eden-logistics <jtmcflea@gmail.com>
2025-10-13 14:02:02 -0700

    add asyncio event loop example

--- chat_pubsub.py
+++ chat_pubsub.py
@@ -1,15 +1,5 @@
 from Pyro5.api import expose
-
-class Publisher():
-    def __init__(self):
-        self.subs: list[Subscriber] = []
-
-    def register(sub: Subscriber):
-        self.subs.append(sub)
-    
-    def publish(self, msg: str):
-        for sub in self.subs:
-            sub.recieve(msg)
+import asyncio
 
 @expose
 class Subscriber():
@@ -19,3 +9,28 @@ class Subscriber():
     def recieve(self, msg):
         print(msg)
 
+class Publisher():
+    def __init__(self):
+        self.subs: list[Subscriber] = []
+
+    def register(self, sub: Subscriber):
+        self.subs.append(sub)
+    
+    def publish(self, msg: str):
+        for sub in self.subs:
+            sub.recieve(msg)
+
+
+async def pub_task(pub: Publisher, sub: Subscriber):
+    pub.register(sub)
+    while (True):
+        msg = input()
+        pub.publish(msg)
+        if msg == "exit":
+            break
+
+pub = Publisher()
+sub = Subscriber()
+loop = asyncio.get_event_loop()
+loop.create_task(pub_task(pub, sub))
+loop.run_forever()
